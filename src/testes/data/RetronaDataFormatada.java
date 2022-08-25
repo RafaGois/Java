ab5e2e6a738ea94f0e7156742e3b2726b1712bb9
rafael.gois <joefontanabg@gmail.com>
2022-08-25 08:28:50 -0300

    commit

--- src/testes/data/RetronaDataFormatada.java
+++ src/testes/data/RetronaDataFormatada.java
@@ -1,13 +1,12 @@
 package testes.data;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class RetronaDataFormatada {
 
     public static void main(String[] args) {
-
+        //esta opcão só é disponível no android a partir da api 26
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
 
         LocalDateTime localDateTime = LocalDateTime.now();
