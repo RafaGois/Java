660727d629f943bc3a9d667cd8503e7ad620eab3
rafael.gois <joefontanabg@gmail.com>
2022-08-26 16:06:26 -0300

    commit

--- src/testes/data/RecebeDataMudaFormato.java
+++ src/testes/data/RecebeDataMudaFormato.java
@@ -1,14 +1,15 @@
 package testes.data;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class RecebeDataMudaFormato {
 
     public static void main(String[] args) {
-        LocalDateTime timeHour = LocalDateTime.parse("2022-08-18T01:30:26");
-
-        System.out.println(timeHour);
-        System.out.println(timeHour.getDayOfMonth());
 
         //fazer logica que recebe uma hora do banco de dados no seguinte formato
         // yyyy-MM-dd hh:mm:ss.sss
@@ -16,5 +17,16 @@ public class RecebeDataMudaFormato {
         // dd-MM-yyyy hh:mm:ss.sss
         //forma de melhor visualizacao para o usuario
         //obs, a hora é retornada do banco como String
+
+        RecebeDataMudaFormato call = new RecebeDataMudaFormato();
+        call.retornaHoraFormatada();
     }
+
+    private void retornaHoraFormatada () {
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate data = LocalDate.parse("23/11/2015", formato);
+        System.out.println(data);
+    }
+
 }
