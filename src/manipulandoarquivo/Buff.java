a42a51236ca86c6d010898f7777be317f0327d50
Rafael Fontana Becker Gois <joefontanabg@gmail.com>
2022-09-23 16:24:02 -0300

    att leitura de arquivos

--- src/manipulandoarquivo/Buff.java
+++ src/manipulandoarquivo/Buff.java
@@ -11,12 +11,9 @@ public class Buff {
     public static void main(String[] args) {
 
         String path = "C:\\Users\\Rafael.Gois\\Documents\\testeJAVA.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
 
             String line = br.readLine();
 
@@ -29,6 +26,4 @@ public class Buff {
             System.out.println("Error: "+e);
         }
     }
-
-
 }
